6099f4d1c8e1fb1603fa60923fb6b0ff4fa16631
cjihrig <cjihrig@gmail.com>
2023-08-14 23:25:20 -0400

    add linter

--- test/index.js
+++ test/index.js
@@ -1,58 +1,58 @@
-'use strict';
-const { deepStrictEqual } = require('node:assert');
-const { test } = require('node:test');
-const { gqlRequest, startRouter } = require('./helper');
+'use strict'
+const { deepStrictEqual } = require('node:assert')
+const { test } = require('node:test')
+const { gqlRequest, startRouter } = require('./helper')
 
 test('proxy a simple single query to a single subgraph', async (t) => {
-  const router = await startRouter(t, ['authors-subgraph']);
+  const router = await startRouter(t, ['authors-subgraph'])
   const query = `
     query {
       list {
         id name { firstName lastName }
       }
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
-    list: [{ id: '1', name: { firstName: 'Peter', lastName: 'Pluck' }}]
-  });
-});
+    list: [{ id: '1', name: { firstName: 'Peter', lastName: 'Pluck' } }]
+  })
+})
 
 test('query with a literal argument', async (t) => {
-  const router = await startRouter(t, ['books-subgraph']);
+  const router = await startRouter(t, ['books-subgraph'])
   const query = `
     query {
       getBook(id: 1) {
         id genre
       }
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
     getBook: { id: '1', genre: 'FICTION' }
-  });
-});
+  })
+})
 
 test('query with a variable argument', async (t) => {
-  const router = await startRouter(t, ['books-subgraph']);
+  const router = await startRouter(t, ['books-subgraph'])
   const query = `
     query GetBookById($id: ID!) {
       getBook(id: $id) {
         id genre
       }
     }
-  `;
-  const data = await gqlRequest(router, query, { id: 2 });
+  `
+  const data = await gqlRequest(router, query, { id: 2 })
 
   deepStrictEqual(data, {
     getBook: { id: '2', genre: 'NONFICTION' }
-  });
-});
+  })
+})
 
 test('nested query with a literal argument', async (t) => {
-  const router = await startRouter(t, ['authors-subgraph']);
+  const router = await startRouter(t, ['authors-subgraph'])
   const query = `
     query {
       list {
@@ -66,8 +66,8 @@ test('nested query with a literal argument', async (t) => {
         }
       }
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
     list: [
@@ -80,11 +80,11 @@ test('nested query with a literal argument', async (t) => {
         todos: [{ task: 'Get really creative' }]
       }
     ]
-  });
-});
+  })
+})
 
 test('nested query with a variable argument', async (t) => {
-  const router = await startRouter(t, ['authors-subgraph']);
+  const router = await startRouter(t, ['authors-subgraph'])
   const query = `
     query GetAuthorListWithTodos($id: ID!) {
       list {
@@ -98,8 +98,8 @@ test('nested query with a variable argument', async (t) => {
         }
       }
     }
-  `;
-  const data = await gqlRequest(router, query, { id: 1 });
+  `
+  const data = await gqlRequest(router, query, { id: 1 })
 
   deepStrictEqual(data, {
     list: [
@@ -112,41 +112,41 @@ test('nested query with a variable argument', async (t) => {
         todos: [{ task: 'Write another book' }]
       }
     ]
-  });
-});
+  })
+})
 
 test('support query aliases', async (t) => {
-  const router = await startRouter(t, ['books-subgraph']);
+  const router = await startRouter(t, ['books-subgraph'])
   const query = `
     query {
       aliasedGetBook: getBook(id: 1) {
         id genre
       }
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
     aliasedGetBook: { id: '1', genre: 'FICTION' }
-  });
-});
+  })
+})
 
 test('scalar return type', async (t) => {
-  const router = await startRouter(t, ['books-subgraph']);
+  const router = await startRouter(t, ['books-subgraph'])
   const query = `
     query {
       getBookTitle(id: 1)
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
     getBookTitle: 'A Book About Things That Never Happened'
-  });
-});
+  })
+})
 
 test('query with meta fields', async (t) => {
-  const router = await startRouter(t, ['books-subgraph']);
+  const router = await startRouter(t, ['books-subgraph'])
   const query = `
     query {
       getBook(id: 1) {
@@ -154,8 +154,8 @@ test('query with meta fields', async (t) => {
         ...on Book { id, genre }
       }
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
     getBook: {
@@ -163,11 +163,11 @@ test('query with meta fields', async (t) => {
       id: '1',
       genre: 'FICTION'
     }
-  });
-});
+  })
+})
 
 test('query with a fragment', async (t) => {
-  const router = await startRouter(t, ['books-subgraph']);
+  const router = await startRouter(t, ['books-subgraph'])
   const query = `
     fragment bookFields on Book { id title genre }
 
@@ -176,8 +176,8 @@ test('query with a fragment', async (t) => {
         ...bookFields
       }
     }
-  `;
-  const data = await gqlRequest(router, query, { id: 1 });
+  `
+  const data = await gqlRequest(router, query, { id: 1 })
 
   deepStrictEqual(data, {
     getBook: {
@@ -185,11 +185,11 @@ test('query with a fragment', async (t) => {
       genre: 'FICTION',
       title: 'A Book About Things That Never Happened'
     }
-  });
-});
+  })
+})
 
 test('resolves a partial entity from a single subgraph', async (t) => {
-  const router = await startRouter(t, ['books-subgraph', 'reviews-subgraph']);
+  const router = await startRouter(t, ['books-subgraph', 'reviews-subgraph'])
   const query = `
     query {
       getReviewBook(id: 1) {
@@ -201,8 +201,8 @@ test('resolves a partial entity from a single subgraph', async (t) => {
         }
       }
     }
-  `;
-  const data = await gqlRequest(router, query);
+  `
+  const data = await gqlRequest(router, query)
 
   deepStrictEqual(data, {
     getReviewBook: {
@@ -215,11 +215,11 @@ test('resolves a partial entity from a single subgraph', async (t) => {
         }
       ]
     }
-  });
-});
+  })
+})
 
 test('resolves an entity across multiple subgraphs', async (t) => {
-  const router = await startRouter(t, ['books-subgraph', 'reviews-subgraph']);
+  const router = await startRouter(t, ['books-subgraph', 'reviews-subgraph'])
 
   await t.test('query flows from non-owner to owner subgraph', async (t) => {
     const query = `
@@ -235,8 +235,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         }
       }
-    `;
-    const data = await gqlRequest(router, query);
+    `
+    const data = await gqlRequest(router, query)
 
     deepStrictEqual(data, {
       getReviewBook: {
@@ -251,8 +251,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         ]
       }
-    });
-  });
+    })
+  })
 
   await t.test('query flows from owner to non-owner subgraph', async (t) => {
     const query = `
@@ -268,8 +268,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         }
       }
-    `;
-    const data = await gqlRequest(router, query);
+    `
+    const data = await gqlRequest(router, query)
 
     deepStrictEqual(data, {
       getBook: {
@@ -284,8 +284,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         ]
       }
-    });
-  });
+    })
+  })
 
   await t.test('fetches foreign key fields not in selection set', async (t) => {
     const query = `
@@ -301,8 +301,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         }
       }
-    `;
-    const data = await gqlRequest(router, query);
+    `
+    const data = await gqlRequest(router, query)
 
     deepStrictEqual(data, {
       getReviewBook: {
@@ -316,8 +316,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         ]
       }
-    });
-  });
+    })
+  })
 
   await t.test('fetches primary key fields not in selection set', async (t) => {
     const query = `
@@ -333,8 +333,8 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         }
       }
-    `;
-    const data = await gqlRequest(router, query);
+    `
+    const data = await gqlRequest(router, query)
 
     deepStrictEqual(data, {
       getBook: {
@@ -348,47 +348,47 @@ test('resolves an entity across multiple subgraphs', async (t) => {
           }
         ]
       }
-    });
-  });
-});
+    })
+  })
+})
 
 test('Mutations', async () => {
   await test('simple mutation', async (t) => {
-    const router = await startRouter(t, ['authors-subgraph']);
+    const router = await startRouter(t, ['authors-subgraph'])
     const query = `
       mutation CreateAuthor($author: AuthorInput!) {
         createAuthor(author: $author) {
           id name { firstName lastName }
         }
       }
-    `;
-    const author = { firstName: 'John', lastName: 'Johnson' };
-    const data = await gqlRequest(router, query, { author });
+    `
+    const author = { firstName: 'John', lastName: 'Johnson' }
+    const data = await gqlRequest(router, query, { author })
 
     deepStrictEqual(data, {
       createAuthor: {
         id: '2',
         name: { firstName: 'John', lastName: 'Johnson' }
       }
-    });
-  });
+    })
+  })
 
   await test('simple mutation with input object literal', async (t) => {
-    const router = await startRouter(t, ['authors-subgraph']);
+    const router = await startRouter(t, ['authors-subgraph'])
     const query = `
       mutation {
         createAuthor(author: { firstName: "Tuco", lastName: "Gustavo" }) {
           id name { firstName lastName }
         }
       }
-    `;
-    const data = await gqlRequest(router, query);
+    `
+    const data = await gqlRequest(router, query)
 
     deepStrictEqual(data, {
       createAuthor: {
         id: '2',
         name: { firstName: 'Tuco', lastName: 'Gustavo' }
       }
-    });
-  });
-});
+    })
+  })
+})
