e7a9a307b30463ceee3ddfd856eff665ed8554ea
cjihrig <cjihrig@gmail.com>
2023-09-07 22:25:38 -0400

    use mercurius client and pr feedback

--- test/subscriptions.js
+++ test/subscriptions.js
@@ -3,7 +3,7 @@ const assert = require('node:assert')
 const { once } = require('node:events')
 const { test } = require('node:test')
 const { setTimeout: sleep } = require('node:timers/promises')
-const { SubscriptionClient } = require('../lib/network')
+const { SubscriptionClient } = require('@mercuriusjs/subscription-client')
 const { gqlRequest, startRouter } = require('./helper')
 
 test('simple subscription', async (t) => {
@@ -15,20 +15,24 @@ test('simple subscription', async (t) => {
 
   await router.listen()
   const wsUrl = `ws://localhost:${router.server.address().port}/graphql`
-  const client = new SubscriptionClient(wsUrl)
+  const client = new SubscriptionClient(wsUrl, { serviceName: 'test' })
 
-  t.after(async () => {
-    await client.close()
+  t.after(() => {
+    client.unsubscribeAll()
+    client.close()
   })
 
-  await client.connect()
-  await client.subscribe(`
+  client.connect()
+  await once(client, 'ready')
+  client.createSubscription(`
     subscription {
       postPublished {
         authorId
       }
     }
-  `)
+  `, {}, (data) => {
+    client.emit('message', data.payload)
+  })
   await sleep(200) // Make sure the subscription has finished setting up.
   const mutation = await gqlRequest(router, `
     mutation {
