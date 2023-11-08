0eea5cfff627818d51137680bdd8bb33fdd88a94
Simone Sanfratello <simone.sanfra@gmail.com>
2023-11-08 13:49:56 +0100

    chore: improve setup (#28)
    * chore: improve setup
    * test: add test runner

---
+++ test/runner.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { tap, spec } = require('node:test/reporters')
+const { run } = require('node:test')
+const glob = require('glob').globSync
+
+/* eslint-disable new-cap */
+const reporter = process.stdout.isTTY ? new spec() : tap
+
+const files = glob('test/**/*.test.js')
+
+run({ files, timeout: 30_000, concurrency: files.length })
+  .compose(reporter)
+  .pipe(process.stdout)
